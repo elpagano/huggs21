1523f5206c8edecd82ff7dc7e2cc02b981001a99
elpagano <estejpagano@gmail.com>
2021-12-30 08:56:58 -0300

    enviromet production true y autentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +9,15 @@ import { Router, ActivatedRoute } from "@angular/router";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(
-    public router: Router,
-  ) {
-    
-  }
+  constructor( public router: Router, public auth: AngularFireAuth, ) 
+  {
 
+  }
+  login() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.auth.signOut();
+  }
   title = 'huggs21';
 }
