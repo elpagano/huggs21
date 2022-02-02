b449605d6bfc48f9357edd70f778ce970ca0fc8c
elpagano <estejpagano@gmail.com>
2022-02-02 12:10:59 -0300

    agregando alta basica y botones basicos

--- src/app/toma-datos/toma-datos.component.ts
+++ src/app/toma-datos/toma-datos.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+
+export interface Item { id: string; name: string; }
 
 @Component({
   selector: 'app-toma-datos',
   templateUrl: './toma-datos.component.html',
   styleUrls: ['./toma-datos.component.css']
 })
-export class TomaDatosComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+export class TomaDatosComponent{
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+  constructor(private readonly afs: AngularFirestore) {
+    this.itemsCollection = afs.collection<Item>('items');
+    this.items = this.itemsCollection.valueChanges({ idField: 'customID' });
   }
-
+  
+  addItem() {
+    const name = "DSFSFDDS";
+    // Persist a document id
+    const id = this.afs.createId();
+    const item: Item = { id, name };
+    this.itemsCollection.doc(id).set(item);
+  }
 }
